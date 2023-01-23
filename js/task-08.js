928f4cb14097175871018025d2a29f4fc5397ac8
Tetiana Nivchyk <tata.02od@gmail.com>
2023-01-23 08:08:49 +0200

    fix task 8

--- js/task-08.js
+++ js/task-08.js
@@ -3,25 +3,28 @@ const input = document.querySelectorAll('input');
 
 const onFormSubmit = function (e) {
   e.preventDefault();
-  [...input].some((el) => el.value === '') && alert('Заповніть пусті поля');
 
-  //Вивести у консоль об'єкт з даними. Метод з elements
-  const elements = e.currentTarget.elements;
-  const email = elements.email.value;
-  const password = elements.password.value;
-  const formData = {
-    email,
-    password,
-  };
-  console.log(formData);
+  if ([...input].some((el) => el.value === '')) {
+    alert('Заповніть пусті поля');
+  } else {
+    //Вивести у консоль об'єкт з даними. Метод з elements
+    const elements = e.currentTarget.elements;
+    const email = elements.email.value;
+    const password = elements.password.value;
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
 
-  //Вивести у консоль об'єкт з даними. Метод з FormData.
-  // const formData = new FormData(e.currentTarget);
-  // const data = {};
-  // formData.forEach((value, name) => {
-  //   data[name] = value;
-  // });
-  // console.log(data);
+    //Вивести у консоль об'єкт з даними. Метод з FormData.
+    // const formData = new FormData(e.currentTarget);
+    // const data = {};
+    // formData.forEach((value, name) => {
+    //   data[name] = value;
+    // });
+    // console.log(data);}
+  }
 };
 
 formRef.addEventListener('submit', onFormSubmit);
